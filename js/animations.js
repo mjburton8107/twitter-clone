2495cd5e98ab4d93b2e9eb054ceef9a9d1bb9db1
MikeB <burton.mj@gmail.com>
2017-02-02 21:00:07 -0700

    in progress

--- js/animations.js
+++ js/animations.js
@@ -4,9 +4,30 @@ $("#tweet-submit").css('display', 'none');
 $("#char-count").css('display', 'none');
 
 $("#tweet-content").on("click", function(){
-  $(".tweet-compose").css("height", "5.5em");
+  $("#user-tweet-compose").css("height", "5.5em");
   $("#tweet-submit").show();
   $("#char-count").show();
+})
+
+$(".tweet-compose").keydown(function(){
+  var charsLeft = 140 - $(".tweet-compose").val().length;
+  $("#char-count").html(charsLeft)
+  if(charsLeft <= 10){
+    $("#char-count").css("color", "red");
+      } else if (charsLeft > 10){
+        $("#char-count").css("color", "black");
+      }
+      if(charsLeft < 0){
+        $("#tweet-submit").hide();
+      }
+      else if (charsLeft >= 0){
+        $("#tweet-submit").show();
+      }
+  });
+
+$("#tweet-submit").on("click", function(){
+  var userInput = $(".tweet-compose").val()
+  $("#stream").prepend(userInput);
 })
 
 })
